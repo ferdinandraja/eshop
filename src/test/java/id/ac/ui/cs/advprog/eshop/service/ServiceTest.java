47e167f7872fa9f433a189ab2a7650eae1c9278f
Ferdinand Raja <ferdinand.raja@ui.ac.id>
2024-02-13 17:32:56 +0700

    sonarcloud revision

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/ServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/ServiceTest.java
@@ -17,7 +17,7 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 
-public class ServiceTest {
+class ServiceTest {
     @Mock
     private ProductRepository productRepository;
 
